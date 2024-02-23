96de23845d8d7bad842f738d621f371875785e06
roninfrodsham <roninfrodsham@gmail.com>
2024-02-23 07:10:42 +0000

    add roboto font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,7 +25,7 @@ function Header({ navOpen, toggleNav, year }: HeaderProps) {
           </button>
         </div>
         <Navigation />
-        <p className='p-6 text-ronin-pink font-montserrat text-sm'>Copyright {year} RoninJS Ltd.</p>
+        <p className='p-6 text-ronin-pink font-roboto text-sm'>Copyright {year} RoninJS Ltd.</p>
       </div>
       <div className='lg:hidden p-6 pt-8 flex flex-row items-center bg-ronin-green-900'>
         <Link to='/' className='grow text-5xl text-ronin-pink'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        montserrat: ["Montserrat", "sans-serif"],
+        roboto: ["Roboto Condensed", "sans-serif"],
       },
     },
   },
